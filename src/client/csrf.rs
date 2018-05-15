66d2649fc25f82d54e73401a087c5962fb8845cf
François Mockers <mockersf@gmail.com>
2018-05-15 19:25:08 +0200

    :white_check_mark: :heavy_plus_sign: test on csrf token with mockito

--- src/client/csrf.rs
+++ src/client/csrf.rs
@@ -34,14 +34,51 @@ impl Jenkins {
         request_builder: &mut RequestBuilder,
     ) -> Result<(), failure::Error> {
         if self.csrf_enabled {
-            let crumb: Crumb = self.get(&Path::CrumbIssuer)?.json()?;
-            if crumb.crumb_request_field != Crumb::header_name() {
-                return Err(errors::Error::InvalidCrumbFieldName {
-                    field_name: crumb.crumb_request_field,
-                }.into());
-            }
-            request_builder.header(crumb);
+            request_builder.header(self.get_csrf()?);
         }
         Ok(())
     }
+
+    pub(crate) fn get_csrf(&self) -> Result<Crumb, failure::Error> {
+        let crumb: Crumb = self.get(&Path::CrumbIssuer)?.json()?;
+        if crumb.crumb_request_field != Crumb::header_name() {
+            return Err(errors::Error::InvalidCrumbFieldName {
+                field_name: crumb.crumb_request_field,
+            }.into());
+        }
+        Ok(crumb)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate mockito;
+
+    static JENKINS_URL: &'static str = mockito::SERVER_URL;
+
+    #[test]
+    fn get_invalid_crumb() {
+        let jenkins_client = ::JenkinsBuilder::new(JENKINS_URL).build().unwrap();
+
+        let _mock = mockito::mock("GET", "/crumbIssuer/api/json")
+            .with_body(
+                r#"
+{
+        "_class":"hudson.security.csrf.DefaultCrumbIssuer",
+        "crumb":"1234567890abcdef",
+        "crumbRequestField":"Invalid-Crumb"
+}
+"#,
+            )
+            .create();
+
+        let crumb = jenkins_client.get_csrf();
+
+        assert!(crumb.is_err());
+        assert_eq!(
+            format!("{:?}", crumb),
+            r#"Err(InvalidCrumbFieldName { field_name: "Invalid-Crumb" })"#
+        );
+    }
+
 }
