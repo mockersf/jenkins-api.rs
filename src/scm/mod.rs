d892f34065f57fb377d384c3b63d932803cfc0cf
François Mockers <mockersf@gmail.com>
2018-05-21 16:03:42 +0200

    :art: clippy suggestion

--- src/scm/mod.rs
+++ src/scm/mod.rs
@@ -1,9 +1,12 @@
 //! Source Control Management configuration
 
+use serde;
+use serde_json;
+
+use helpers::Class;
+
 mod browser;
 pub use self::browser::*;
-mod scm;
-pub use self::scm::*;
 
 /// SCM merge options
 #[derive(Default, Deserialize, Debug)]
@@ -18,3 +21,42 @@ pub struct MergeOptions {
     /// Remote branch
     remote_branch_name: Option<String>,
 }
+
+/// Trait implemented by specialization of SCM
+pub trait SCM {}
+
+/// A SCM
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct CommonSCM {
+    /// _class provided by Jenkins
+    #[serde(rename = "_class")]
+    pub class: Option<String>,
+
+    #[serde(flatten)]
+    other_fields: serde_json::Value,
+}
+specialize!(CommonSCM => SCM);
+impl SCM for CommonSCM {}
+
+/// No SCM
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct NullSCM {
+    /// Browser
+    pub browser: Option<CommonBrowser>,
+}
+register_class!("hudson.scm.NullSCM" =>  NullSCM);
+impl SCM for NullSCM {}
+
+/// Git SCM
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct GitSCM {
+    /// Browser
+    pub browser: Option<CommonBrowser>,
+    /// Merge options
+    pub merge_options: MergeOptions,
+}
+register_class!("hudson.plugins.git.GitSCM" =>  GitSCM);
+impl SCM for GitSCM {}
